2c6fce5cdd24174a6856bec8a7a09419464e6e5e
Sean Ross <40174061@live.napier.ac.uk>
2015-09-17 22:02:46 +0100

    Part 3c
    Splits a string and adds the values

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,56 +10,19 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            bool isPrime = true;
-            List<int> primeNumbers = new List<int>();
-            for (int i = 1; i > 0; i++)
-            {
-                Console.WriteLine("Please enter a number between 1 and 10,000");
-                num = int.Parse(Console.ReadLine());
-                if (num > 1 && num < 10000)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Error");
-                }
-            }
-            for (int j = 2; j < num; j++)
-            {
-                if (j <= 3)
-                {
-                    primeNumbers.Add(j);
-                }
-                else
-                {
-                    int boundary = (int)Math.Floor(Math.Sqrt(j));
+            int total = 0;
+            Console.WriteLine("Enter numbers to add up, separated by a single comma (,)");
+            string s = (Console.ReadLine());
+            string[] numbers = s.Split(',');
 
-                    for (int k = 2; k <= boundary; ++k)
-                    {
-                        if (j % k == 0)
-                        {
-                            isPrime = false;
-                            break;
-                        }
-                        else
-                        {
-                            isPrime = true;
-                        }
-                    }
-                    if (isPrime == true)
-                    {
-                        primeNumbers.Add(j);
-                    }
-                }
-            }
-            Console.Write("These are all the prime numbers between 1 and " + num + ": ");
-            for (int i = 0; i < primeNumbers.Count; i++)
+            int[] convertedItems = Array.ConvertAll<string, int>(numbers, int.Parse);
+
+            for (int i = 0; i < convertedItems.Length; i++)
             {
-                Console.Write(primeNumbers[i] + ", ");
+                total += convertedItems[i];
             }
-            Console.WriteLine();
+
+            Console.WriteLine("The total is " + total);
             Console.ReadKey();
         }
     }
