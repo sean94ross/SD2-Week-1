1b8a83588ec4e239b64c3e203fa5212d9ee6a414
Sean Ross <40174061@live.napier.ac.uk>
2015-09-17 18:14:49 +0100

    Part 2a

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Press return to exit");
+            Console.ReadKey();
+        }
+    }
+}
