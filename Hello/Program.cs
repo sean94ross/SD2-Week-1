5fd21102bfbb88acfae30a3e8c41be002d731600
Sean Ross <40174061@live.napier.ac.uk>
2015-09-17 18:17:30 +0100

    Part 3a
    app to calculate area of a house

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,10 +10,39 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name");
-            String name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-            Console.WriteLine("Press return to exit");
+            string roomName;
+            int totalArea = 0;
+            List<string> roomNames = new List<string>();
+            List<int> lengths = new List<int>();
+            List<int> widths = new List<int>();
+            Console.WriteLine("Enter the Room Name, Length and Width of each room in the house, when finished enter 'Done'");
+            for (int i = 1; i > 0; i++)
+            {
+                Console.Write("Enter the room name: ");
+                roomName = Console.ReadLine();
+                if (roomName.Equals("Done"))
+                {
+                    break;
+                }
+                else
+                {
+                    roomNames.Add(roomName);
+                }
+                Console.Write("Enter the room length: ");
+                lengths.Add(int.Parse(Console.ReadLine()));
+                Console.Write("Enter the room width: ");
+                widths.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+            Console.WriteLine(String.Format("{0, -20} {1,-10}", "Room", "Area"));
+            for (int i = 0; i < lengths.Count; i++)
+            {
+                int area = lengths[i] * widths[i];
+                totalArea += area;
+                Console.WriteLine(String.Format("{0,-20} {1,-10}", roomNames[i], area));
+            }
+            Console.WriteLine();
+            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Total", totalArea));
             Console.ReadKey();
         }
     }
