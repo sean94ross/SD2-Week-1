9f56a04ce9e4bbe56823a0b0ec1b76cef3431f21
Sean Ross <40174061@live.napier.ac.uk>
2015-09-17 20:00:51 +0100

    Part 3b
    App to check prime numbers

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,39 +10,56 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string roomName;
-            int totalArea = 0;
-            List<string> roomNames = new List<string>();
-            List<int> lengths = new List<int>();
-            List<int> widths = new List<int>();
-            Console.WriteLine("Enter the Room Name, Length and Width of each room in the house, when finished enter 'Done'");
+            int num = 0;
+            bool isPrime = true;
+            List<int> primeNumbers = new List<int>();
             for (int i = 1; i > 0; i++)
             {
-                Console.Write("Enter the room name: ");
-                roomName = Console.ReadLine();
-                if (roomName.Equals("Done"))
+                Console.WriteLine("Please enter a number between 1 and 10,000");
+                num = int.Parse(Console.ReadLine());
+                if (num > 1 && num < 10000)
                 {
                     break;
                 }
                 else
                 {
-                    roomNames.Add(roomName);
+                    Console.WriteLine("Error");
                 }
-                Console.Write("Enter the room length: ");
-                lengths.Add(int.Parse(Console.ReadLine()));
-                Console.Write("Enter the room width: ");
-                widths.Add(int.Parse(Console.ReadLine()));
             }
-            Console.WriteLine();
-            Console.WriteLine(String.Format("{0, -20} {1,-10}", "Room", "Area"));
-            for (int i = 0; i < lengths.Count; i++)
+            for (int j = 2; j < num; j++)
             {
-                int area = lengths[i] * widths[i];
-                totalArea += area;
-                Console.WriteLine(String.Format("{0,-20} {1,-10}", roomNames[i], area));
+                if (j <= 3)
+                {
+                    primeNumbers.Add(j);
+                }
+                else
+                {
+                    int boundary = (int)Math.Floor(Math.Sqrt(j));
+
+                    for (int k = 2; k <= boundary; ++k)
+                    {
+                        if (j % k == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                        else
+                        {
+                            isPrime = true;
+                        }
+                    }
+                    if (isPrime == true)
+                    {
+                        primeNumbers.Add(j);
+                    }
+                }
+            }
+            Console.Write("These are all the prime numbers between 1 and " + num + ": ");
+            for (int i = 0; i < primeNumbers.Count; i++)
+            {
+                Console.Write(primeNumbers[i] + ", ");
             }
             Console.WriteLine();
-            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Total", totalArea));
             Console.ReadKey();
         }
     }
